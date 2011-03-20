6a717c9215b72dd147911041be43ecbc6f3960fd
Juan David Gonzalez Cobas <dcobas@cern.ch>
2011-03-20 18:09:21 +0100

    encore: remove pyvme related stuff
    Now the ./driver path contains exactly the driver+support library
    stuff and nothing else, i.e., the invariable part of an encore driver.
    This can be copied freely to construct an encore driver file structure

--- driver/cvorg_install.sh
+++
@@ -1,8 +0,0 @@
-#!	/bin/sh
-insmod vmeio.ko \
-        vme1=0x800  amd1=0x29 win1=0x400 dwd1=4 \
-	vme2=0x8100 amd2=0x29 win2=0x100 dwd2=2 \
-	vecs=0x83 lvls=2 luns=0
-
-MAJOR=` grep vmeio /proc/devices | awk '{ print $1 }' `
-mknod /dev/vmeio.0 c $MAJOR 0
