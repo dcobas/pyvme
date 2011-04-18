54c9cfded79e71cb6f3821713c2c476b8689e2c8
Juan David Gonzalez Cobas <dcobas@cern.ch>
2011-04-18 10:11:32 +0200

    encore: shell script examples for Frederic Dubouchet

---
+++ encore/rf_csv.sh
@@ -0,0 +1,28 @@
+#!	/bin/sh
+
+sqlplus -s copub/co@dbabco <<QUERY
+set echo off;
+set verify off;
+set feed off;
+set pagesize 0;
+set pause off;
+set linesize 4000;
+set trims on;
+select name || ',' ||
+    rwmode || ',' ||
+    block || ',' ||
+    mb.address || ',' ||
+    nvl(mb.offsetval,0) || ',' ||
+    mr.offsetval || ',' ||
+    (mr.offsetval + nvl(mb.offsetval,0)) || ',' ||
+    wordsize || ',' ||
+    depthval || ',' ||
+    nvl(mr.description, '')
+from moduleregisters mr join moduleblocks mb
+    using (block, moduletype_id)
+where moduletype_id = (
+    select hwtype_id
+    from hard_types
+    where hwtype = '$1' )
+order by block;
+QUERY
