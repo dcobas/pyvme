0b32a6eae5d195937db33dc7be2ddb45535a08bc
Juan David Gonzalez Cobas <dcobas@cern.ch>
2011-04-24 16:35:41 +0200

    encore: move inline templates to ./templates directory
    Consequently, the configuration for the template names is
    made global and rearranged

---
+++ encore/templates/install.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# Generated automatically by install.py at %(date)s"
+echo "Installing %(device_name)s driver..."
+
+INSMOD_ARGS=`awk -f transfer2insmod.awk %(device_name)s %(transfer)s`
+
+if [ x"$INSMOD_ARGS" == x"luns=" ] ; then
+    echo "No %(device_name)s declared in %(transfer)s, exiting"
+    exit 1
+fi
+
+echo "installing %(driver_name)s by insmod $INSMOD_ARGS"
+insmod %(driver_name)s.ko "$INSMOD_ARGS"
+
+MAJOR=`cat /proc/devices | awk '$2 == "%(driver_name)s" {print $1}' %(transfer)s`
+if [ -z "$MAJOR" ]; then
+	echo "driver %(driver_name)s not installed!"
+	exit 1
+fi
+
+MINORS=`awk '/^#+#/ && $6 == "%(device_name)s" { print $7, }' %(transfer)s`
+echo "creating device nodes for driver %(driver_name)s, major $MAJOR, minors $MINORS"
+for MINOR in $MINORS; do
+    rm -f /dev/%(driver_name)s.$MINOR
+    mknod /dev/%(driver_name)s.$MINOR c $MAJOR $MINOR
+done
