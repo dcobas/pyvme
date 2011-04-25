b4b8deedeae2d08b18a62f4c60b5eeb2a560b751
Juan David Gonzalez Cobas <dcobas@cern.ch>
2011-04-25 18:16:56 +0200

    encore: indent correctly register names

--- encore/templates/module_regs.h
+++ encore/templates/module_regs.h
@@ -19,7 +19,7 @@ extern struct encore_reginfo %(driver_name)s_registers[];
 extern int %(driver_name)s_nregs;
 
 enum %(driver_name)s_register_id {
-	%(register_names)s
+%(register_names)s
 };
 
 #endif /* _%(device_name)s_REGS_H */
