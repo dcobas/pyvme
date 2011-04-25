3ef69e9c8a25a464b52eee4bf6f65ee746c2e9d7
Juan David Gonzalez Cobas <dcobas@cern.ch>
2011-04-25 13:03:01 +0200

    encore: generate a module_regs.h and module_regs.c register  info lib

---
+++ encore/templates/module_regs.h
@@ -0,0 +1,25 @@
+#ifndef _%(device_name)s_REGS_H
+#define _%(device_name)s_REGS_H
+
+struct encore_reginfo {
+	char	name[17];
+	char	rwmode[5];
+	int	block;
+	int	block_address_space;
+	int	block_offset;
+	int	register_offset;
+	int	offset;
+	char	wordsize[7];
+	int	depth;
+	char	description[81];
+	int	data_width;
+};
+
+extern struct encore_reginfo %(driver_name)s_registers[];
+extern int %(driver_name)s_nregs;
+
+enum rf_vtu_register_id {
+	%(register_names)s
+};
+
+#endif /* _%(device_name)s_REGS_H */
