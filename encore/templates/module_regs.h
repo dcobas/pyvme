6dc605a7fb692816811ad259d07a0ff84ad45482
Juan David Gonzalez Cobas <dcobas@cern.ch>
2011-04-25 20:13:44 +0200

    vmeio/encore: move struct encore_reginfo declaration to vmeio

--- encore/templates/module_regs.h
+++ encore/templates/module_regs.h
@@ -1,19 +1,7 @@
 #ifndef _%(device_name)s_REGS_H
 #define _%(device_name)s_REGS_H
 
-struct encore_reginfo {
-	char	name[17];
-	char	rwmode[5];
-	int	block;
-	int	block_address_space;
-	int	block_offset;
-	int	register_offset;
-	int	offset;
-	char	wordsize[7];
-	int	depth;
-	char	description[81];
-	int	data_width;
-};
+/* struct encore_reginfo definition needed */
 
 extern struct encore_reginfo %(driver_name)s_registers[];
 extern int %(driver_name)s_nregs;
