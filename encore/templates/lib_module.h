a05551eb862c54b787698b04373764a6be8804eb
Juan David Gonzalez Cobas <dcobas@cern.ch>
2011-07-01 16:34:59 +0200

    encore: make header files C++-friendly

--- encore/templates/lib_module.h
+++ encore/templates/lib_module.h
@@ -1,1 +1,16 @@
 /* lib_%(driver_name)s API */
+
+#ifndef _LIB_%(device_name)s_H
+#define _LIB_%(device_name)s_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* %(driver_name)s lib declarations */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _%(device_name)s_REGS_H */
--- encore/templates/module_regs.h
+++ encore/templates/module_regs.h
@@ -1,6 +1,10 @@
 #ifndef _%(device_name)s_REGS_H
 #define _%(device_name)s_REGS_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 /* struct encore_reginfo definition needed */
 
 extern struct encore_reginfo %(driver_name)s_registers[];
@@ -10,4 +14,7 @@ enum %(driver_name)s_register_id {
 %(register_names)s
 };
 
+#ifdef __cplusplus
+}
+#endif
 #endif /* _%(device_name)s_REGS_H */
